4bd056b8df87aa1b50853d1edffa12bc6f78eb42
Alexander Anichkin <13101979@ukr.net>
2020-05-22 18:58:06 +0300

    Time.h: add fields and methods; Time.cpp: add empty methods

--- Time/Time.cpp
+++ Time/Time.cpp
@@ -1,1 +1,154 @@
+// Include files
 #include "Time.h"
+
+Time::Time(int hour, int minute, int second)
+{
+}
+
+Time::Time(const s_hour* my_hours)
+{
+}
+
+Time::Time(const s_minute* my_minutes)
+{
+}
+
+Time::Time(const s_second* my_seconds)
+{
+}
+
+Time::Time(const Time& Time_incoming)
+{
+}
+
+Time::Time(const Time&& Time_incoming)
+{
+}
+
+Time::~Time()
+{
+}
+
+Time& Time::operator=(const Time& Time_incoming)
+{
+	// TODO: вставьте здесь оператор return
+}
+
+Time& Time::operator=(const Time&& Time_incoming)
+{
+	// TODO: вставьте здесь оператор return
+}
+
+Time Time::operator+(Time& Time_incoming)
+{
+	return Time();
+}
+
+Time Time::operator+(const s_hour* my_hours)
+{
+	return Time();
+}
+
+Time Time::operator+(const s_minute* my_minutes)
+{
+	return Time();
+}
+
+Time Time::operator+(const s_second* my_seconds)
+{
+	return Time();
+}
+
+Time Time::operator-(Time& Time_incoming)
+{
+	return Time();
+}
+
+Time Time::operator-(const s_hour* my_hours)
+{
+	return Time();
+}
+
+Time Time::operator-(const s_minute* my_minutes)
+{
+	return Time();
+}
+
+Time Time::operator-(const s_second* my_seconds)
+{
+	return Time();
+}
+
+void Time::SetHour(int hour)
+{
+}
+
+void Time::SetMinute(int minute)
+{
+}
+
+void Time::SetSecond(int second)
+{
+}
+
+void Time::SetTypeClock(int typeclock)
+{
+}
+
+void Time::SetAM_PM(int am_pm)
+{
+}
+
+void Time::SetTypeTime(int type_time)
+{
+}
+
+int Time::GetHour()
+{
+	return 0;
+}
+
+int Time::GetMinute()
+{
+	return 0;
+}
+
+int Time::GetSecond()
+{
+	return 0;
+}
+
+int Time::GetTypeClock()
+{
+	return 0;
+}
+
+int Time::GetAMPM()
+{
+	return 0;
+}
+
+int Time::GetTypeTime()
+{
+	return 0;
+}
+
+void Time::ChangeTimeFormat()
+{
+}
+
+void Time::ChangeTimeZone(int hour, int minute)
+{
+}
+
+void Time::ClockChange()
+{
+}
+
+void Time::InputTime(int hour, int minute, int second)
+{
+}
+
+void Time::ShowTime()
+{
+}
--- Time/Time.h
+++ Time/Time.h
@@ -1,8 +1,71 @@
 #ifndef _TIME_H_
 #define _TIME_H_
 
+// Structures
+// Hour
+struct s_hour
+{
+	int hour;
+};
+// Minute
+struct s_minute
+{
+	int minute;
+};
+// Second
+struct s_second
+{
+	int second;
+};
+
+// Class Time
 class Time
 {
+public:
+	Time() : _hour{ 0 }, _minute{ 0 }, _second{ 0 }, _type_clock{ 1 }, _am_pm{ 0 }, _type_time{ 1 } {};		// Constructor without parameters
+	Time(int hour, int minute, int second = 0);																// Constructor with parameters
+	Time(const s_hour * my_hours);																			// Constructor with parameters
+	Time(const s_minute * my_minutes);																		// Constructor with parameters
+	Time(const s_second * my_seconds);																		// Constructor with parameters
+	Time(float hour, float minute, float second) = delete;													// Ñonstructor call barring
+	Time(double hour, double minute, double second) = delete;												// Ñonstructor call barring
+	Time(const Time & Time_incoming);																		// Copt constructor
+	Time(const Time && Time_incoming);																		// Transfer constructor
+	~Time();																								// Destructor
+	Time & operator = (const Time & Time_incoming);															// Copy assignment operator overload
+	Time & operator = (const Time && Time_incoming);														// Transfer assignment operator overload
+	Time operator + (Time & Time_incoming);																	// Overload operator + (object + object)
+	Time operator + (const s_hour * my_hours);																// Overload operator + (object + hours)
+	Time operator + (const s_minute * my_minutes);															// Overload operator + (object + minutes)
+	Time operator + (const s_second * my_seconds);															// Overload operator + (object + seconds)
+	Time operator - (Time & Time_incoming);																	// Overload operator - (object - object)
+	Time operator - (const s_hour * my_hours);																// Overload operator - (object - hours)
+	Time operator - (const s_minute * my_minutes);															// Overload operator - (object - minutes)
+	Time operator - (const s_second * my_seconds);															// Overload operator - (object - seconds)
+	void SetHour(int hour);																					// Set hours
+	void SetMinute(int minute);																				// Set minutes
+	void SetSecond(int second);																				// Set seconds
+	void SetTypeClock(int typeclock);																		// Set clock type
+	void SetAM_PM(int am_pm);																				// Set midday and midnight designations
+	void SetTypeTime(int type_time);																		// Set type time
+	int GetHour();																							// Get hours
+	int GetMinute();																						// Get minutes
+	int GetSecond();																						// Get seconds
+	int GetTypeClock();																						// Get clock type
+	int GetAMPM();																							// Get midday and midnight designations
+	int GetTypeTime();																						// Get type time
+	void ChangeTimeFormat();																				// Change time format
+	void ChangeTimeZone(int hour, int minute = 0);															// Change time zone
+	void ClockChange();																						// Clock change (switch to winter / summer time)
+	void InputTime(int hour, int minute, int second = 0);													// Input time
+	void ShowTime();																						// Show time
+private:
+	int _hour;																								// Hours
+	int _minute;																							// Minutes
+	int _second;																							// Seconds
+	int _type_clock;																						// Clock type (1 - 24 hours format, 2 - 12 hours format)
+	int _am_pm;																								// Midday and midnight designations (1 - a.m., 2 - p.m.)
+	int _type_time;																							// Type of time (1 - summer time, 2 - winter time)
 };
 
 #endif
