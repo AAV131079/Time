684d13e006493e94a2550e67da9f5be03703d3f7
Alexander Anichkin <13101979@ukr.net>
2020-05-22 19:10:40 +0300

    Time.cpp: added implementation of constructors and destructor

--- Time/Time.cpp
+++ Time/Time.cpp
@@ -1,33 +1,195 @@
 // Include files
 #include "Time.h"
 
-Time::Time(int hour, int minute, int second)
+// Macroses
+#define DAY 24
+#define HALF_DAY 12
+#define HOUR 60
+#define MINUTE 60
+#define AM " a.m."
+#define PM " p.m."
+
+// Namespace std
+using namespace std;
+
+// Constructor (hour + minutes + stcond (default = 0))
+Time :: Time(int hour, int minute, int second)
 {
+	// Check hours
+	if (hour >= 0 && hour < DAY)
+	{
+		// Set hours
+		this -> SetHour(hour);
+	}
+	// Check minutes
+	if (minute >= 0 && minute < HOUR)
+	{
+		// Set minutes
+		this -> SetMinute(minute);
+	}
+	// Check second
+	if (second >= 0 && second < MINUTE)
+	{
+		// Set seconds
+		this -> SetSecond(second);
+	}
+	// Type clock
+	this -> SetTypeClock(1);
+	// Midday and midnight designations
+	this -> SetAM_PM(0);
+	// Type time
+	this -> SetTypeTime(1);
 }
 
-Time::Time(const s_hour* my_hours)
+// Constructor (hour)
+Time :: Time(const s_hour * my_hours)
 {
+	// Set hours
+	this -> SetHour(my_hours -> hour);
+	// Set minutes
+	this -> SetMinute(0);
+	// Set seconds
+	this -> SetSecond(0);
+	// Type clock
+	this -> SetTypeClock(1);
+	// Midday and midnight designations
+	this -> SetAM_PM(0);
+	// Type time
+	this -> SetTypeTime(1);
 }
 
-Time::Time(const s_minute* my_minutes)
+// Constructor (minutes)
+Time :: Time(const s_minute * my_minutes)
 {
+	// Variables
+	int hours{};
+	// Check minutes
+	if (my_minutes -> minute < HOUR)
+	{
+		// Set minutes
+		this -> SetMinute(my_minutes -> minute);
+		// Set hours
+		this -> SetHour(0);
+	}
+	else if (my_minutes -> minute >= HOUR)
+	{
+		// How many hours
+		hours = (int)(my_minutes -> minute / HOUR);
+		// Set minutes
+		this -> SetMinute(my_minutes -> minute - HOUR * hours);
+		// Set hours
+		this -> SetHour(hours);
+	}
+	// Set seconds
+	this -> SetSecond(0);
+	// Type clock
+	this -> SetTypeClock(1);
+	// Midday and midnight designations
+	this -> SetAM_PM(0);
+	// Type time
+	this -> SetTypeTime(1);
 }
 
-Time::Time(const s_second* my_seconds)
+// Constructor (second)
+Time :: Time(const s_second * my_seconds)
 {
+	// Variables
+	int hours{}, minutes{};
+	// Check seconds
+	if (my_seconds -> second < MINUTE)
+	{
+		// Set seconds
+		this -> SetSecond(my_seconds -> second);
+		// Set minutes
+		this -> SetMinute(0);
+		// Set hours
+		this -> SetHour(0);
+	}
+	else if (my_seconds -> second >= MINUTE)
+	{
+		// How many minutes
+		minutes = (int)(my_seconds -> second / MINUTE);
+		// Seconds
+		this -> SetSecond(my_seconds -> second - MINUTE * minutes);
+		// Check minutes
+		if (minutes < HOUR)
+		{
+			// Set minutes
+			this -> SetMinute(minutes);
+			// Set hours
+			this -> SetHour(0);
+		}
+		else if (minutes >= HOUR)
+		{
+			// How many hours
+			hours = (int)(minutes / HOUR);
+			// Set minutes
+			this -> SetMinute(minutes - hours * HOUR);
+			// Set hours
+			this -> SetHour(hours);
+		}
+	}
+	// Type clock
+	this -> SetTypeClock(1);
+	// Midday and midnight designations
+	this -> SetAM_PM(0);
+	// Type time
+	this -> SetTypeTime(1);
 }
 
-Time::Time(const Time& Time_incoming)
+// Copy constructor
+Time :: Time(const Time & Time_incoming)
 {
+	// Hour
+	this -> SetHour(Time_incoming._hour);
+	// Minute
+	this -> SetMinute(Time_incoming._minute);
+	// Second
+	this -> SetSecond(Time_incoming._second);
+	// Type clock
+	this -> SetTypeClock(Time_incoming._type_clock);
+	// Midday and midnight designations
+	this -> SetAM_PM(Time_incoming._am_pm);
+	// Type time
+	this -> SetTypeTime(Time_incoming._type_time);
 }
 
-Time::Time(const Time&& Time_incoming)
+// Transfer constructor
+Time :: Time(const Time && Time_incoming)
 {
+	// Hour
+	this -> SetHour(Time_incoming._hour);
+	// Minute
+	this -> SetMinute(Time_incoming._minute);
+	// Second
+	this -> SetSecond(Time_incoming._second);
+	// Type clock
+	this -> SetTypeClock(Time_incoming._type_clock);
+	// Midday and midnight designations
+	this -> SetAM_PM(Time_incoming._am_pm);
+	// Type time
+	this -> SetTypeTime(Time_incoming._type_time);
+	// Destruct incoming object
+	Time_incoming.~Time();
 }
 
-Time::~Time()
+// Destructor
+Time :: ~Time()
 {
+	// Hour
+	this -> _hour = 0;
+	// Minute
+	this -> _minute = 0;
+	// Second
+	this -> _second = 0;
+	// Type clock
+	this -> _type_clock = 0;
+	// Midday and midnight designations
+	this -> _am_pm = 0;
+	// Type time
+	this -> _type_time = 0;
 }
+
 
 Time& Time::operator=(const Time& Time_incoming)
 {
